d5a82cfb324ee4d7c4625c7c31b5a53f63b6fe39
nikkureev <32942796+nikkureev@users.noreply.github.com>
2020-02-06 12:56:06 +0300

    Update manhattan_plor.r

--- manhattan_plor.r
+++ manhattan_plor.r
@@ -1,6 +1,6 @@
-pldf = read.table('plink5.assoc.linear', header = T)
-
 library(qqman)
 library(lattice)
+
+pldf = read.table('plink5.assoc.linear', header = T)
 
 m = manhattan(pldf, ylim = c(0, 10), chr = 'CHR', genomewideline = 6.02, suggestiveline = F)
